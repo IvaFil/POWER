205ef577e6c6a53b7872db7d622f5d87f7fc0aaf
IvaFil <filipovichiv@yandex.ru>
2022-07-21 17:24:30 +0300

    Среднее арифметическое

---
+++ cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace cs
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, str2;
+            
+            Console.WriteLine("Введи число 1: ");
+            str = Console.ReadLine();
+            double a = Convert.ToInt64(str);
+
+            Console.WriteLine("Введи число 2: ");
+            str2 = Console.ReadLine();
+            double b = Convert.ToInt64(str2);
+
+            double result = (a + b) / 2;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
